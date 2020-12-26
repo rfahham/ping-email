923d64f009b2594dabb648e3882c81abccd43775
ricardo.fahham <ricardo.fahham@g.globo>
2020-12-26 14:05:59 -0300

    subindo projeto

---
+++ ping+email.py
@@ -0,0 +1,53 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+
+import os
+import smtplib
+import getpass
+import threading 
+from time import *
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from threading import Timer
+
+cores = {'vermelho':'\033[31m','verde':'\033[32m',}
+hostname = "192.168.0.1"
+response = os.system("ping -c 1 " + hostname)
+beep = os.system( "say Server is DOWN" )
+
+def enviar_email():
+	
+	email = ""
+	# password = getpass.getpass('Digite sua senha: ')
+	password = ""
+	send_to_email = ""
+	subject = "Teste de conexão com o servidor"
+	message = "O servidor parou de responder!"
+		
+	msg = MIMEMultipart()
+	msg['From'] = email
+	msg['To'] = send_to_email
+	msg['Subject'] = subject
+		
+	msg.attach(MIMEText(message, 'html'))
+	
+	server = smtplib.SMTP('SMTP.office365.com', 587)
+	server.starttls()
+	server.login(email, password)
+	text = msg.as_string()
+	server.sendmail(email, send_to_email, text)
+	server.quit()
+
+def validar():
+	if response == 0:
+		print("")
+		print('\033[32mServer is UP !!!\033[m ')
+	else:
+		enviar_email()
+		print('\033[31mServer is DOWN !!!\033[m ')
+		beep
+	sleep(300)
+
+while True:
+	validar()
+
